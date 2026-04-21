598ee0ecb6501337d84f5b653d2f22274b2ac0c1
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-04-21 16:12:08 +0530

    Component placement updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ const App = () => {
     <div className="fixed top-0 left-0 -z-10 w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
     <NavBar />
     <HeroSection />
-    <Education />
     <Skills />
-    <Experience />
     <Project />
+    <Experience />
+    <Education />
     <Certificates />
     <Footer />
     </>
